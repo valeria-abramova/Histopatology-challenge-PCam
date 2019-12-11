dc50b241a99417229fb33a999817456df40a58bc
valeria-abramova <58753818+valeria-abramova@users.noreply.github.com>
2019-12-11 15:23:31 +0500

    uncommented lines

--- project_hist.py
+++ project_hist.py
@@ -186,135 +186,135 @@ def mser_feature(image):
 
     return np.reshape(np.asarray(msers).shape[0],1)
 
-# # grab the list of images that we'll be describing
-# print("[INFO] describing images...")
-# imagePaths = list(paths.list_images("C:\\Users\\z\\Desktop\\UdG\\CAD\\hist data\\train\\m0"))
-# imagePaths.extend(list(paths.list_images("C:\\Users\\z\\Desktop\\UdG\\CAD\\hist data\\train\\b0")))
-# random.shuffle(imagePaths)
+# grab the list of images that we'll be describing
+print("[INFO] describing images...")
+imagePaths = list(paths.list_images("C:\\Users\\z\\Desktop\\UdG\\CAD\\hist data\\train\\m0"))
+imagePaths.extend(list(paths.list_images("C:\\Users\\z\\Desktop\\UdG\\CAD\\hist data\\train\\b0")))
+random.shuffle(imagePaths)
+
+# grab validation paths
+val_paths = list(paths.list_images("C:\\Users\\z\\Desktop\\UdG\\CAD\\hist data\\val\\m0"))
+val_paths.extend(list(paths.list_images("C:\\Users\\z\\Desktop\\UdG\\CAD\\hist data\\val\\b0")))
+random.shuffle(val_paths)
+
+
+# initialize the features matrix and labels list
+
+features = []
+labels = []
+features_val=[]
+labels_val=[]
 #
-# # grab validation paths
-# val_paths = list(paths.list_images("C:\\Users\\z\\Desktop\\UdG\\CAD\\hist data\\val\\m0"))
-# val_paths.extend(list(paths.list_images("C:\\Users\\z\\Desktop\\UdG\\CAD\\hist data\\val\\b0")))
-# random.shuffle(val_paths)
-#
-#
-# # initialize the features matrix and labels list
-#
-# features = []
-# labels = []
-# features_val=[]
-# labels_val=[]
-#
-# #path to the extracted bovw and extracting image indexes in a list
-#
-# bovwDB = h5py.File("C:\\Users\\z\\Desktop\\UdG\\CAD\\hist\\bovw.hdf5")
-# featuresDB = h5py.File("C:\\Users\\z\\Desktop\\UdG\\CAD\\hist\\sumVal_features.hdf5", mode="r")
-# list_ids = [x for x in featuresDB["image_ids"]]
-#
-# bovwDB_val = h5py.File("C:\\Users\\z\\Desktop\\UdG\\CAD\\hist\\bovw_val.hdf5")
-# featuresDB_val = h5py.File("C:\\Users\\z\\Desktop\\UdG\\CAD\\hist\\sumVal_features_valid_set.hdf5", mode="r")
-# list_ids_val = [x for x in featuresDB_val["image_ids"]]
-#
-# # source image for color normalization
-# source = cv2.imread("C:\\Users\\z\\Desktop\\UdG\\CAD\\hist data\\train\\m_try\\m_trainImage98.png", cv2.IMREAD_UNCHANGED)
-#
-# # loop over the input images
-# for (i, imagePath) in enumerate(imagePaths):
-#     # read the image
-#     image = cv2.imread(imagePath, cv2.IMREAD_UNCHANGED)
-#
-#     # extract image id and image label
-#     imageID = imagePath.split(os.path.sep)[-1].split(".")[0]
-#     label = imagePath.split(os.path.sep)[-1].split("_")[0]
-#
-#     if label == "b":
-#         label = 0
-#     elif label == "m":
-#         label = 1
-#
-#     # find index of the current image in the features file
-#     index = list_ids.index(imageID)
-#
-#     # color normalization
-#     image = color_transfer.color_transfer(source,image)
-#
-#     #extract features
-#     hist = extract_color_histogram(image)
-#     hist_rgb = extract_color_histogram(cv2.cvtColor(image, cv2.COLOR_HSV2RGB))
-#
-#     glcm1 = np.reshape(glcm_feature(image[:, :, 2])[0], 1)
-#     glcm2 = np.reshape(glcm_feature(image[:, :, 2])[1], 1)
-#     glcm3 = np.reshape(glcm_feature(image[:, :, 2])[2], 1)
-#
-#     ts = tas(image)
-#
-#     col_stat1 = np.asarray(color_statistics(image)[0]).flatten()
-#     col_stat2 = np.asarray(color_statistics(image)[1]).flatten()
-#
-#     col_stat_rgb1 = np.asarray(color_statistics(skimage.color.hsv2rgb(image))[0]).flatten()
-#     col_stat_rgb2 = np.asarray(color_statistics(skimage.color.hsv2rgb(image))[1]).flatten()
-#
-#     hist = np.concatenate((hist, hist_rgb,  col_stat1, col_stat2, col_stat_rgb1, col_stat_rgb2,glcm1, glcm2, glcm3, ts, bovwDB["bovw"][index]))
-#     features.append(hist)
-#     labels.append(label)
-#
-#     # show an update every 1,000 images
-#     if i > 0 and i % 1000 == 0:
-#         print("[INFO] processed {}/{}".format(i, len(imagePaths)))
-#
-# features = np.array(features)
-# labels = np.array(labels)
-#
-# trainFeat = features
-# trainLabels = labels
-#
-# for (i, valPath) in enumerate(val_paths):
-#     # load the image and extract the class label (assuming that our
-#     # path as the format: /path/to/dataset/{class}.{image_num}.jpg
-#     image = cv2.imread(valPath, cv2.IMREAD_UNCHANGED)
-#     imageID = valPath.split(os.path.sep)[-1].split(".")[0]
-#
-#     index = list_ids_val.index(imageID)
-#
-#     label = valPath.split(os.path.sep)[-1].split("_")[0]
-#
-#     if label == "b":
-#         label = 0
-#     elif label == "m":
-#         label = 1
-#
-#     image = color_transfer.color_transfer(source, image)
-#
-#     hist = extract_color_histogram(image)
-#
-#     glcm1 = np.reshape(glcm_feature(image[:, :, 2])[0], 1)
-#     glcm2 = np.reshape(glcm_feature(image[:, :, 2])[1], 1)
-#     glcm3 = np.reshape(glcm_feature(image[:, :, 2])[2], 1)
-#
-#     ts = tas(image)
-#     hist_rgb = extract_color_histogram(cv2.cvtColor(image, cv2.COLOR_HSV2RGB))
-#
-#     col_stat1 = np.asarray(color_statistics(image)[0]).flatten()
-#     col_stat2 = np.asarray(color_statistics(image)[1]).flatten()
-#
-#     col_stat_rgb1 = np.asarray(color_statistics(skimage.color.hsv2rgb(image))[0]).flatten()
-#     col_stat_rgb2 = np.asarray(color_statistics(skimage.color.hsv2rgb(image))[1]).flatten()
-#
-#
-#     hist = np.concatenate((hist, hist_rgb, col_stat1, col_stat2, col_stat_rgb1, col_stat_rgb2, glcm1, glcm2, glcm3,
-#                             ts, bovwDB_val["bovw"][index]))
-#
-#     features_val.append(hist)
-#     labels_val.append(label)
-#
-#     # show an update every 1,000 images
-#     if i > 0 and i % 1000 == 0:
-#         print("[INFO] processed {}/{}".format(i, len(val_paths)))
-#
-# features_val = np.array(features_val)
-# labels_val = np.array(labels_val)
-#
-# testFeat = features_val
-# testLabels = labels_val
-#
-# Forest_classifier_ROC(trainFeat, trainLabels, testFeat, testLabels)
+#path to the extracted bovw and extracting image indexes in a list
+
+bovwDB = h5py.File("C:\\Users\\z\\Desktop\\UdG\\CAD\\hist\\bovw.hdf5")
+featuresDB = h5py.File("C:\\Users\\z\\Desktop\\UdG\\CAD\\hist\\sumVal_features.hdf5", mode="r")
+list_ids = [x for x in featuresDB["image_ids"]]
+
+bovwDB_val = h5py.File("C:\\Users\\z\\Desktop\\UdG\\CAD\\hist\\bovw_val.hdf5")
+featuresDB_val = h5py.File("C:\\Users\\z\\Desktop\\UdG\\CAD\\hist\\sumVal_features_valid_set.hdf5", mode="r")
+list_ids_val = [x for x in featuresDB_val["image_ids"]]
+
+# source image for color normalization
+source = cv2.imread("C:\\Users\\z\\Desktop\\UdG\\CAD\\hist data\\train\\m_try\\m_trainImage98.png", cv2.IMREAD_UNCHANGED)
+
+# loop over the input images
+for (i, imagePath) in enumerate(imagePaths):
+    # read the image
+    image = cv2.imread(imagePath, cv2.IMREAD_UNCHANGED)
+
+    # extract image id and image label
+    imageID = imagePath.split(os.path.sep)[-1].split(".")[0]
+    label = imagePath.split(os.path.sep)[-1].split("_")[0]
+
+    if label == "b":
+        label = 0
+    elif label == "m":
+        label = 1
+
+    # find index of the current image in the features file
+    index = list_ids.index(imageID)
+
+    # color normalization
+    image = color_transfer.color_transfer(source,image)
+
+    #extract features
+    hist = extract_color_histogram(image)
+    hist_rgb = extract_color_histogram(cv2.cvtColor(image, cv2.COLOR_HSV2RGB))
+
+    glcm1 = np.reshape(glcm_feature(image[:, :, 2])[0], 1)
+    glcm2 = np.reshape(glcm_feature(image[:, :, 2])[1], 1)
+    glcm3 = np.reshape(glcm_feature(image[:, :, 2])[2], 1)
+
+    ts = tas(image)
+
+    col_stat1 = np.asarray(color_statistics(image)[0]).flatten()
+    col_stat2 = np.asarray(color_statistics(image)[1]).flatten()
+
+    col_stat_rgb1 = np.asarray(color_statistics(skimage.color.hsv2rgb(image))[0]).flatten()
+    col_stat_rgb2 = np.asarray(color_statistics(skimage.color.hsv2rgb(image))[1]).flatten()
+
+    hist = np.concatenate((hist, hist_rgb,  col_stat1, col_stat2, col_stat_rgb1, col_stat_rgb2,glcm1, glcm2, glcm3, ts, bovwDB["bovw"][index]))
+    features.append(hist)
+    labels.append(label)
+
+    # show an update every 1,000 images
+    if i > 0 and i % 1000 == 0:
+        print("[INFO] processed {}/{}".format(i, len(imagePaths)))
+
+features = np.array(features)
+labels = np.array(labels)
+
+trainFeat = features
+trainLabels = labels
+
+for (i, valPath) in enumerate(val_paths):
+    # load the image and extract the class label (assuming that our
+    # path as the format: /path/to/dataset/{class}.{image_num}.jpg
+    image = cv2.imread(valPath, cv2.IMREAD_UNCHANGED)
+    imageID = valPath.split(os.path.sep)[-1].split(".")[0]
+
+    index = list_ids_val.index(imageID)
+
+    label = valPath.split(os.path.sep)[-1].split("_")[0]
+
+    if label == "b":
+        label = 0
+    elif label == "m":
+        label = 1
+
+    image = color_transfer.color_transfer(source, image)
+
+    hist = extract_color_histogram(image)
+
+    glcm1 = np.reshape(glcm_feature(image[:, :, 2])[0], 1)
+    glcm2 = np.reshape(glcm_feature(image[:, :, 2])[1], 1)
+    glcm3 = np.reshape(glcm_feature(image[:, :, 2])[2], 1)
+
+    ts = tas(image)
+    hist_rgb = extract_color_histogram(cv2.cvtColor(image, cv2.COLOR_HSV2RGB))
+
+    col_stat1 = np.asarray(color_statistics(image)[0]).flatten()
+    col_stat2 = np.asarray(color_statistics(image)[1]).flatten()
+
+    col_stat_rgb1 = np.asarray(color_statistics(skimage.color.hsv2rgb(image))[0]).flatten()
+    col_stat_rgb2 = np.asarray(color_statistics(skimage.color.hsv2rgb(image))[1]).flatten()
+
+
+    hist = np.concatenate((hist, hist_rgb, col_stat1, col_stat2, col_stat_rgb1, col_stat_rgb2, glcm1, glcm2, glcm3,
+                            ts, bovwDB_val["bovw"][index]))
+
+    features_val.append(hist)
+    labels_val.append(label)
+
+    # show an update every 1,000 images
+    if i > 0 and i % 1000 == 0:
+        print("[INFO] processed {}/{}".format(i, len(val_paths)))
+
+features_val = np.array(features_val)
+labels_val = np.array(labels_val)
+
+testFeat = features_val
+testLabels = labels_val
+
+Forest_classifier_ROC(trainFeat, trainLabels, testFeat, testLabels)
